1001f50e9c2eb5753610c4279661f20d171f0425
Wesley Meneghini <wesley.meneghini@outlook.com.br>
2019-11-16 00:31:37 -0300

    Ajustando front-end da paginas do cliente

--- curiosidades.php
+++ curiosidades.php
@@ -21,6 +21,8 @@
         <?php
             require_once('menu.php');
         ?>
+        <div class="tamanho_cabecalho">
+        </div>
 
         <!-- Caixa com imagem de fundo e tema da pagina -->
         <section id="banner_curiosidades">
@@ -38,34 +40,47 @@
             <div class="conteudo center">
                 
                 <?php 
+
                     $sql = "select * from tbl_curiosidades";
-                
                     $select = mysqli_query($conexao, $sql);
                 
                     while($rsCuriosidades = mysqli_fetch_array($select)){
+
                 ?>
                 
                 <!-- faixa com conteudo de uma imagem e um texto de curiosidades -->
                 <div class="faixa_curiosidades">
                     <div class="img_curiosidades">
                         <figure>
-                            <img src="bd/imagens/<?=$rsCuriosidades['imagem']?>" alt="sucos naturais" class="bkg-img">
+
+                            <img 
+                                src="bd/imagens/<?=$rsCuriosidades['imagem']?>" 
+                                alt="sucos naturais" 
+                                class="bkg-img">
+
                         </figure>
                     </div>
+
                     <div class="conteudo_curiosidades">
+
                         <!-- titulo da curiosidade -->
                         <h1 class="txt_titulo">
+
                             <?=$rsCuriosidades['titulo']?>
 <!--                            Benefícios de fazer um suco natural ao invés de consumir os industrializados-->
                         </h1>
 
                         <!-- texto com as curiosidades -->
                         <p class="txt_curiosidades">
+
                             <?=$rsCuriosidades['conteudo']?>
+
                         </p>
                         
                     </div>
+
                     <div class="clear"></div>
+
                 </div>
                 
                 <?php 
@@ -73,11 +88,14 @@
                 ?>
 
             </div>
+
         </section>
 
         <!-- importar rodape -->
         <?php
             require_once('rodape.php');
         ?>
+
     </body>
+    
 </html>
--- empresa.php
+++ empresa.php
@@ -17,71 +17,99 @@
     </head>
     <body>
 
-        <!--  -->
         <?php
             require_once('menu.php');
         ?>
+        <div class="tamanho_cabecalho">
+        </div>
+
         <section id="sobre_a_empresa">
-            <div class="opacity">
+
+            <div class="opacity-bkg">
+
                 <div class="conteudo center">
 
                     <?php
-                        $sql = "select * from tbl_empresa;";
 
+                        $sql = "select * from tbl_empresa;";
                         $select = mysqli_query($conexao, $sql);
 
                         while($rsEmpresa = mysqli_fetch_array($select)){
 
-                        
                     ?>
 
-                    <h1 
-                        class="titulo center-txt">
+                    <!-- Titulo -->
+                    <h1 class="is-1 center-txt">
+
                         <?=$rsEmpresa['titulo']?>
+                            
                     </h1>
+
+                    <!-- Texto -->
                     <h2 class="texto_esplicativo center-txt center">
+
                         <?=$rsEmpresa['conteudo']?>
+
                     </h2>
                     
                     <?php
                         }
 
                         $sqlCard = "select * from tbl_empresa_card;";
-
                         $selectCard = mysqli_query($conexao, $sqlCard);
 
                         while($rsEmpresaCard = mysqli_fetch_array($selectCard)){
-
                         
                     ?>
 
                         
 
                     <div class="missao_visao_valore">
-                        <div class="icone_sobre_empresa center">
-                            <figure>
-                                <img src="bd/imagens/<?=$rsEmpresaCard['imagem']?>" alt="missao" class="bkg-img">
-                            </figure>
-                        </div>
-                        <p class="p_missao center-txt negrito">
-                        <?=$rsEmpresaCard['titulo']?>
-                        </p>
-                        <p class="center-txt txt-size-20">
-                            <?=$rsEmpresaCard['conteudo']?>
-                        </p>
-                    </div>
 
+                        <div class="icone_sobre_empresa center">
+
+                            <figure>
+
+                                <!-- Imagem do card -->
+                                <img 
+                                    src="bd/imagens/<?=$rsEmpresaCard['imagem']?>" 
+                                    alt="missao" 
+                                    class="bkg-img">
+                            
+                            </figure>
+
+                        </div>
+
+                        <p class="p_missao center-txt negrito">
+
+                            <?=$rsEmpresaCard['titulo']?>
+
+                        </p>
+
+                        <p class="center-txt txt-size-20">
+
+                            <?=$rsEmpresaCard['conteudo']?>
+
+                        </p>
+
+                    </div>
 
                     <?php
                         }
                     ?>
                     
                     <div class="clear"></div>
+
                 </div>
+
             </div>
+
         </section>
+
         <?php
             require_once('rodape.php');
         ?>
+
     </body>
+    
 </html>
